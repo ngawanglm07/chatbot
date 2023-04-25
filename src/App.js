8c5bf44310b65de0fba424d245a66caa37d100e6
Your Name <you@example.com>
2023-04-25 16:04:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
+import ChatBot from 'react-simple-chatbot';
 import './App.css';
+import {Segment} from 'semantic-ui-react';
+
 
 function App() {
+
+  const steps = [ 
+    {
+      id:'Greet',
+      message:"Hello welcome to our chat app" ,
+      trigger:"ask name"
+    } , {
+      id:'ask name',
+      message:"please enter your name",
+      trigger:"waiting"
+    } ,{
+      id:'waiting',
+      user: true,
+    trigger: 'Name'
+    } , {
+      id:'Name',
+      message:'HI {previousValue} , please select your issue',
+      trigger: 'issues'
+    } , {
+      id:'issues',
+      options:[
+        {
+          value:'React',
+          label:"React", 
+          trigger:"React"
+        },  {
+          value:'Angular',
+          label:"Angular", 
+          trigger:"Angular"
+        }
+       
+      ]
+    }
+    ,
+    {
+      id:'React',
+      message:"thanks for letting us know your issue", 
+      trigger:"email",
+    } ,
+    {
+      id:'Angular',
+      message:"thanks for letting us know your issue" ,
+      trigger:'email',
+    } ,
+    {
+      id:'email',
+      message:'please enter your email now',
+      trigger:'thanks',
+    } , 
+    {
+      id:'thanks',
+      user: true,
+      trigger:'ben',
+
+    } , {
+      id:'ben',
+      message:'thanks we will mail you shortly',
+      end:true
+    }
+  ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='floa'>
+  <Segment >
+<ChatBot steps={steps} className="chat"/>
+  </Segment>
+  <footer className='foot'> © Copyright Ngawang_lm 2023</footer>
     </div>
+
   );
 }
 
